ab3fac8a8d2610fc426fbd73c032c6cffafc052e
Kennedy DeSousa <kennedydesousa@gmail.com>
2018-02-13 13:10:13 -0800

    flask connected with alchemy

--- database_setup.py
+++ database_setup.py
@@ -1,7 +1,5 @@
-
 import os
 import sys
-import sqlalchemy
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -9,26 +7,23 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
-
+ 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
-    name = Column(String(80), nullable=False)
-    id = Column(Integer, primary_key=True)
+    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
-    ## Add the engine and base  to see if it will correct problem
-    engine = create_engine('sqlite:///restaurantmenu.db')
 
-    Base.metadata.create_all(engine)
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
